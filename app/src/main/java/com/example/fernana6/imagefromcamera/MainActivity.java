2dee9299351bd21fde5af2b244d7f62baada3c92
EU\FernanA6 <alberto.fernandezseral@unisys.com>
2018-05-03 17:24:21 +0200

    Final

--- app/src/main/java/com/example/fernana6/imagefromcamera/MainActivity.java
+++ app/src/main/java/com/example/fernana6/imagefromcamera/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.fernana6.imagefromcamera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageHolder;
+    private final int requestCode = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageHolder = findViewById(R.id.img);
+
+        Button capturedImageButton = findViewById(R.id.btnChange);
+        capturedImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+
+        final Button storeImage = findViewById(R.id.btnStore);
+        storeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    storePicture();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void takePicture(){
+        Intent photoCaptureIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(photoCaptureIntent, requestCode);
+    }
+
+    private void storePicture() throws IOException {
+        //to get the image from the ImageView (say iv)
+        BitmapDrawable draw = (BitmapDrawable) imageHolder.getDrawable();
+        Bitmap bitmap = draw.getBitmap();
+
+        FileOutputStream outStream = null;
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdCard.getAbsolutePath() + "/YourFolderName");
+        dir.mkdirs();
+        String fileName = String.format("%d.png", System.currentTimeMillis());
+        File outFile = new File(dir, fileName);
+        outStream = new FileOutputStream(outFile);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+        outStream.flush();
+        outStream.close();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(this.requestCode == requestCode && resultCode == RESULT_OK){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageHolder.setImageBitmap(bitmap);
+        }
     }
 }
